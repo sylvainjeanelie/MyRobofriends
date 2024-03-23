2700f18ff7478d54c2462e2f56bf5302225f4d8a
sylvainjeanelie <sylvainjeanelie@gmail.com>
2024-03-23 11:38:35 +0800

    Update robot size

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card = ({name, email}) => {
     
     return (
         <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-            <img alt="robots" src={`https://robohash.org/${name}?200x200`}/>
+            <img alt="robots" src={`https://robohash.org/${name}?size=200x200`}/>
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
